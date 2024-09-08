42d4590449ff02c4ecc21636e9dd1a76151e31af
greghori <e.souala@esi-sba.dz>
2024-09-08 04:09:09 +0100

    fix notifications event

--- auth/app/Events/NewNotifications.php
+++ auth/app/Events/NewNotifications.php
@@ -2,24 +2,24 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NewNotifications
+class NewNotifications implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $user_id;
     /**
      * Create a new event instance.
      */
-    public function __construct(public string $user_id)
+    public function __construct($user_id)
     {
         //
+        $this->user_id = $user_id;
     }
 
     /**
--- auth/routes/channels.php
+++ auth/routes/channels.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('notifications.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return  $user->id ===  $id;
 });
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return  $user->id ===  $id;
 });
